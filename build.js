f9a28e99cd17d9750f3c45ee05bc4659c6f46aa0
Pablo Garcia <pgarcia@onfocusdev.com>
2020-08-01 00:18:14 -0300

    Add base files

---
+++ build.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const fs = require('fs-extra');
+const childProcess = require('child_process');
+
+try {
+  // Remove current build
+  fs.removeSync('./dist/');
+
+  // Transpile the typescript files
+  childProcess.exec('tsc --build tsconfig.prod.json');
+} catch (err) {
+  console.log(err);
+}
