d7b44fd23e89bd6012cb41188a033d4adc1b594c
Pablo Garcia <pgarcia@onfocusdev.com>
2020-08-03 00:43:20 -0300

    Add return doc for remove route

--- src/controllers/sensors/remove-one.ts
+++ src/controllers/sensors/remove-one.ts
@@ -6,7 +6,7 @@ export default function removeSensor(req: Request, res: Response): void {
   const { id } = req.params;
 
   SensorDao.removeOne(id)
-    .then(() => res.sendStatus(200))
+    .then((doc) => res.status(200).json(doc))
     .catch((err) =>
       handleResErrors(err, () =>
         res.status(500).json({
